9e624180220251b9f5d37ad00ca334ae32695fe7
bl81hi <fussel@fussel.de>
2026-01-03 15:54:32 +0100

    Added GetValues example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use kuksa::val::v2::val_client::ValClient;
 use kuksa::val::v2::GetValueRequest;
+use kuksa::val::v2::GetValuesRequest;
 use kuksa::val::v2::SignalId; 
 use kuksa::val::v2::signal_id::Signal;
 
@@ -19,12 +20,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = ValClient::connect(addr).await?;
     println!("Connected to KUKSA VAL v2 Broker at address {}", addr);
 
-    // Create request - Vehicle.Speed
-    let request = tonic::Request::new(GetValueRequest {
+    // Create a single value request - Vehicle.Speed
+    let request = GetValueRequest {
         signal_id: Some(SignalId {
             signal: Some(Signal::Path("Vehicle.Speed".to_string())),
         }),
-    });
+    };
 
     match client.get_value(request).await {
         Ok(response) => {
@@ -33,6 +34,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("Success! Received data point {:?}", dp.value);
             }
         },
+        Err(e) => println!("Error: {}", e),
+    }
+
+    // Now we create a request for several Signals in one call
+    let signals = vec![
+        SignalId {
+            signal: Some(Signal::Path("Vehicle.Speed".to_string())),
+        },
+        SignalId {
+            signal: Some(Signal::Path("Vehicle.Chassis.Axle.Row1.Wheel.Left.Tire.Pressure".to_string())),
+        },
+    ];
+
+    let request = GetValuesRequest {
+        signal_ids: signals,
+    };
+
+    match client.get_values(request).await {
+        Ok(response) => {
+            let reply = response.into_inner();
+            for (index, dp) in reply.data_points.iter().enumerate() {
+                if let Some(val) = &dp.value {
+                    println!("Value #{}: {:?}", index, val);
+                } else {
+                    println!("Value #{}: Datapoint has no value set", index);
+                }
+            }
+        }
         Err(e) => println!("Error: {}", e),
     }
 
